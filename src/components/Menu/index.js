3c78726d9408a1631a333458630df109a40db5b2
yagoantonio <yago_antonio@live.com>
2020-07-28 21:16:03 -0300

    Alterações Aula 2

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -2,15 +2,16 @@ import React from 'react'
 import Logo from '../../assets/img/Logo.png'
 import './menu.css'
 import Button from '../Button'
+import { Link } from 'react-router-dom'
 
 function Menu() {
     return (
         <nav className="Menu">
-            <a href="/">
+            <Link to="/">
                 <img className="Logo" src={Logo} alt="YagoFlix logo" />
-            </a>
+            </Link>
 
-            <Button as="a" className="ButtonLink"  href="/">
+            <Button as={Link} className="ButtonLink"  to="/cadastro/video">
                 Novo Vídeo
             </Button>
         </nav>
